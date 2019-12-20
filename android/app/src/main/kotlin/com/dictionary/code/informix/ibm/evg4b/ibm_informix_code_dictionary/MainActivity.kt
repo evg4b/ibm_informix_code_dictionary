45279125621182b7355a976704d49367ae88d31c
Evgeny Abramovich <evg.abramovitch@gmail.com>
2019-12-20 18:12:35 +0300

    Init commit

---
+++ android/app/src/main/kotlin/com/dictionary/code/informix/ibm/evg4b/ibm_informix_code_dictionary/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dictionary.code.informix.ibm.evg4b.ibm_informix_code_dictionary
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
